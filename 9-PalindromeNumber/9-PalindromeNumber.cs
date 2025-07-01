8d3a25598da115fe38d3206b10248469703e7e25
Fares El-Basousy <80477030+Fares-Basousy@users.noreply.github.com>
2025-07-01 15:31:46 +0300

    File updated at 7/1/2025, 3:32:35 PM

--- 9-PalindromeNumber/9-PalindromeNumber.cs
+++ 9-PalindromeNumber/9-PalindromeNumber.cs
@@ -1,19 +1,22 @@
-// Last updated: 7/1/2025, 3:02:45 PM
+// Last updated: 7/1/2025, 3:32:35 PM
 public class Solution {
-    public static string Reverse( string s )
-    {
-    char[] charArray = s.ToCharArray();
-    Array.Reverse(charArray);
-    return new string(charArray);
+    public static int Reverse( int s )
+    { int Rev = 0;
+    while (s > 0){
+        Rev *= 10;
+        Rev +=  s%10;
+        s /= 10;
+    }
+    Console.WriteLine(Rev);
+    return Rev;
     }
     public bool IsPalindrome(int x) {
         if (x <0){
             return false;
         }
         else{
-            string XString = x.ToString();
-            string RevString = Reverse(XString);
-            return XString == RevString ? true : false ;
+            int RevX = Reverse(x);
+            return x == RevX ? true : false ;
 
             
         }
