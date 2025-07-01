3ce56bf8805f693ea5f5ea33fa7594199fefa71c
Fares El-Basousy <80477030+Fares-Basousy@users.noreply.github.com>
2025-07-01 17:50:12 +0300

    File updated at 7/1/2025, 5:51:01 PM

--- 9-PalindromeNumber/9-PalindromeNumber.cs
+++ 9-PalindromeNumber/9-PalindromeNumber.cs
@@ -1,4 +1,4 @@
-// Last updated: 7/1/2025, 5:50:26 PM
+// Last updated: 7/1/2025, 5:51:01 PM
 public class Solution {
     public string[] IntoSubstrings (string s){
         char[] CharArray = s.ToCharArray();
@@ -6,16 +6,14 @@ public class Solution {
         int StartingPoint = 0;
         List<string> NoDup = new List<string>();
         for (int i = 0; i < CharArray.Length; i++) {
-           // Console.WriteLine(i);
             if(!LoopVar.Contains(CharArray[i]))
                 LoopVar += CharArray[i];
             else{
                 NoDup.Add(new string(LoopVar));
                 StartingPoint +=1 ; 
-                //Console.WriteLine(i);
-                //Console.WriteLine(StartingPoint);
+    
                 i = StartingPoint-1;
-                //Console.WriteLine(string.Join(", ", NoDup));
+                
                 LoopVar = "";
             }
         }
