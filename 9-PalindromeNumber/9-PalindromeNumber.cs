767cf0d1779aa2d61231d4eac860dc546d677097
Fares El-Basousy <80477030+Fares-Basousy@users.noreply.github.com>
2025-07-01 17:49:37 +0300

    File updated at 7/1/2025, 5:50:26 PM

--- 9-PalindromeNumber/9-PalindromeNumber.cs
+++ 9-PalindromeNumber/9-PalindromeNumber.cs
@@ -1,25 +1,34 @@
-// Last updated: 7/1/2025, 3:32:35 PM
+// Last updated: 7/1/2025, 5:50:26 PM
 public class Solution {
-    public static int Reverse( int s )
-    { int Rev = 0;
-    while (s > 0){
-        Rev *= 10;
-        Rev +=  s%10;
-        s /= 10;
-    }
-    Console.WriteLine(Rev);
-    return Rev;
-    }
-    public bool IsPalindrome(int x) {
-        if (x <0){
-            return false;
+    public string[] IntoSubstrings (string s){
+        char[] CharArray = s.ToCharArray();
+        string LoopVar = "";
+        int StartingPoint = 0;
+        List<string> NoDup = new List<string>();
+        for (int i = 0; i < CharArray.Length; i++) {
+           // Console.WriteLine(i);
+            if(!LoopVar.Contains(CharArray[i]))
+                LoopVar += CharArray[i];
+            else{
+                NoDup.Add(new string(LoopVar));
+                StartingPoint +=1 ; 
+                //Console.WriteLine(i);
+                //Console.WriteLine(StartingPoint);
+                i = StartingPoint-1;
+                //Console.WriteLine(string.Join(", ", NoDup));
+                LoopVar = "";
+            }
         }
-        else{
-            int RevX = Reverse(x);
-            return x == RevX ? true : false ;
-
-            
+        NoDup.Add(new string(LoopVar)); //Put last substring
+        return NoDup.ToArray();
+    }
+    public int LengthOfLongestSubstring(string s) {
+        string[] NoDups = IntoSubstrings(s);
+        int Longest = 0;
+        for (int i = 0; i < NoDups.Length; i++){
+            if(NoDups[i].Length > Longest)
+                Longest = NoDups[i].Length;
         }
-        
+        return Longest;
     }
 }
