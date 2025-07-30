6d879a7eac30fb01dbe31043589cbaf61a237490
Fares El-Basousy <80477030+Fares-Basousy@users.noreply.github.com>
2025-07-30 15:08:44 +0300

    Create 146-LRUCache

---
+++ 146-LRUCache/146-LRUCache.cs
@@ -0,0 +1,56 @@
+// Last updated: 7/30/2025, 3:08:43 PM
+public class LRUCache {
+    private int DicCapacity ;
+    private List<int> UsedKeys  ;
+    private List<int> InsertedKeys  ;
+    private Dictionary <int,int> cache ;
+    public LRUCache(int capacity) {
+        DicCapacity = capacity;
+        cache = new Dictionary<int, int>();
+        UsedKeys = new List<int>(); 
+        InsertedKeys = new List<int>(); 
+    }
+    
+    public int Get(int key) {
+        if( cache.ContainsKey(key) ){
+            UsedKeys.Remove(key);
+            UsedKeys.Insert(0,key);
+            if (UsedKeys.Count > DicCapacity){
+                int LRU = UsedKeys[DicCapacity];
+                UsedKeys.Remove(LRU);
+                cache.Remove(LRU);
+               }
+               
+            return cache[key];
+            }
+        else
+            return -1;
+    }
+    
+    public void Put(int key, int value) {
+        if(cache.ContainsKey(key)){
+            cache[key] = value;
+            UsedKeys.Remove(key);     
+            UsedKeys.Insert(0,key);
+            return  ;
+        }
+        else if(cache.Count == DicCapacity){
+            int OldestKey = UsedKeys.Last() ;
+            UsedKeys.Remove(OldestKey);                
+            cache.Remove(OldestKey);
+        }
+        cache.Add(key,value);
+        UsedKeys.Insert(0,key);
+                        
+        
+        
+    }
+
+}
+
+/**
+ * Your LRUCache object will be instantiated and called as such:
+ * LRUCache obj = new LRUCache(capacity);
+ * int param_1 = obj.Get(key);
+ * obj.Put(key,value);
+ */
