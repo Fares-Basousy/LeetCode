f5b953ebdbc43b18b3143b59773b1827bf034ef5
Fares El-Basousy <80477030+Fares-Basousy@users.noreply.github.com>
2025-08-01 17:57:23 +0300

    File updated at 8/1/2025, 5:57:23 PM

--- 155-MinStack/155-MinStack.cs
+++ 155-MinStack/155-MinStack.cs
@@ -1,4 +1,4 @@
-// Last updated: 8/1/2025, 5:56:50 PM
+// Last updated: 8/1/2025, 5:57:23 PM
 using System.Linq;
 public class MinStack {
     private List<int> stack;
@@ -9,22 +9,18 @@ public class MinStack {
     }
     
     public void Push(int val) {
-        Console.WriteLine(val);
         if(min.Count == 0 || min.Last() >= val)
             min.Add(val);
-        Console.WriteLine($"Pushed {val}");
         stack.Add(val);        
     }
     
     public void Pop() {
         int value = stack.Last();
         stack.RemoveAt(stack.Count -1);
-        Console.WriteLine($"popped {value} remaining {stack.Count}");
 
 
         if(min.Last() == value){
             min.RemoveAt(min.Count -1);
-          //  Console.WriteLine($"pop min {min.Last()} remaining {min.Count}");
             }
     }
     
@@ -34,7 +30,6 @@ public class MinStack {
     }
     
     public int GetMin() {
-        Console.WriteLine($"getMin min count:{min.Count} last item : {min.Last()}");
         return min.Last();
         
     }
